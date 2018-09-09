903cfe5b732cc4d9440ebc518fa3fee9318485cb
huddy987 <huddy987@gmail.com>
2018-09-08 21:20:11 -0600

    Added loading screens

--- js/randomurl.js
+++ js/randomurl.js
@@ -4,12 +4,13 @@ function randomurl(...urls){
   var urlchoice = (Math.floor(Math.random() * (arguments.length)));
   //Convert the randomly chosen url to a string
   var urlstring = String(arguments[urlchoice]);
-  console.log(urlchoice);
+  //Comment for debugging: console.log(urlchoice);
   $(document).ready(function(){
     $("#randclick").attr("href", urlstring);
     //When the URL is clicked, redirect to the random URL.
     $("#randclick").click(function(){
         $("#randclick").attr("href", urlstring);
+        //When the URL is clicked, generate a new random URL
         urlchoice = (Math.floor(Math.random() * (arguments.length)));
         urlstring = String(arguments[urlchoice]);
       });
